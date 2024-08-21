851d0adec0758edb544f5630f94432cc0eee7d9c
nicolebayani <nicolebayani110@gmail.com>
2024-08-22 00:40:34 +0800

    Add files via upload

---
+++ Lab 1 Update.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+	cout << "---------" << endl;
+	cout << "Main Menu" << endl;
+	cout << "---------" << endl;
+	
+	do{
+	
+		cout << "[1]-List of Files" << endl;
+		cout << "[2]-Create a New Directory" << endl;
+		cout << "[3]-Change the Working Directory" << endl;
+		cout << "[4]-Exit" << endl; //to terminate the program
+		cout << "Enter the number you have chosen: ";
+		cin >> choice; // so you can choose in the main menu
+		
+		switch (choice){
+			case 1: cout << "[1]All Files" << endl;
+					cout << "[2]Extension Files" << endl;
+					cout << "[3]Name Wise" << endl;
+					
+					if (1 == choice){
+						cout << "file1.txt" << endl;
+						cout << "file2.cpp" << endl;
+						cout << "report.doc" << endl;
+						cout << "image.jpg" << endl;
+					}
+					break;
+		}
+	}
+	while(choice != choice);
+}
