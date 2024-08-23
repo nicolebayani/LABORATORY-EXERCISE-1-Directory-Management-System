0de47fac5317443099aabdf00f819b1186fafa7e
nicolebayani <nicolebayani110@gmail.com>
2024-08-24 01:11:16 +0800

    Add files via upload
    I started from the top again because I just recently read that the code must contain appropriate C++ libraries

--- Lab 1 Update.cpp
+++ Lab 1 Update.cpp
@@ -1,14 +1,47 @@
 #include <iostream>
-#include <filesystem>
 #include <string>
+#include <direct.h>
+#include <windows.h>
 
 using namespace std;
 
-void ListofFiles(){
-	if(fs:: exist(path) && fs::directory(path)){
-		cout << "Files "
+void ListFiles(String DirectPath, String Filter = "" ){
+	FindData FindFileData;
+	HandleFind = FirstFile((DirectPath + "\\*").c_str(), FindFileData);
+	
+	if  (HandleFind == InvalidHandleVal){
+		cout << "Cannot access directory: " << DirectPath << endl;
+		return;
 	}
+	
+	do {
+        string FileName = FindFileData.cFileName;
+        if (filter.empty() || FileName.find(filter) != string::npos) {
+            cout << FileName << endl;
+        }
+    } while (NextFile(HandleFind, FindFileData) != 0);
+
+    FindClose(HandleFind);
 }
+
+void CreateDirectory(String DirectName) {
+    if (MakeDirect(DirectName.c_str()) == 0) {
+        cout << "New Directory created successfully." << endl;
+    } else {
+        cout << "Failed to new create directory. It may already exist or there was an error." << endl;
+    }
+}
+
+void ChangeDirectory(const string& path) {
+    if (ChanDirect(path.c_str()) == 0) {
+        char cwd[256];
+        getcwd(cwd, sizeof(cwd));
+        cout << "Current directory changed to: " << cwd << endl;
+    } else {
+        cout << "Error changing directory to: " << path << endl;
+    }
+}
+
 
 
 
